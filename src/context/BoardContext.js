81a9e0b70e09edb6c784221bf62a3b3e2ae3b4e2
Robert Reed <robert.mc.reed@gmail.com>
2019-05-14 22:56:02 -0700

    Calculate num changes onClick and pass as lastFill

--- src/context/BoardContext.js
+++ src/context/BoardContext.js
@@ -1,15 +1,33 @@
-import React, { useState, createContext, useMemo } from 'react'
+import React, { useState, createContext, useMemo, useCallback } from 'react'
 
 import { blankBoard } from '../lib/floodFill'
 
-export const ItemsContext = createContext([])
+const nullContext = { board: blankBoard, setBoard: () => {} }
+
+export const ItemsContext = createContext(nullContext)
 
 const BoardProvider = ({ children }) => {
   const [board, setBoard] = useState(blankBoard)
+  const [lastFill, setLastFill] = useState(0)
+
+  const handleUpdateBoard = useCallback((updatedBoard) => {
+      let count = 0;
+
+      board.forEach((row, y) => row.forEach(({ color }, x) => {
+        if (color !== updatedBoard[y][x].color) count++
+      }))
+
+      setLastFill(count)
+      setBoard(updatedBoard)
+    },
+    [board],
+  )
+
   const itemsHandler = useMemo(() => ({
     board,
-    setBoard,
-  }), [board])
+    lastFill,
+    setBoard: handleUpdateBoard,
+  }), [board, lastFill, handleUpdateBoard])
 
   return (
     <ItemsContext.Provider value={itemsHandler}>
